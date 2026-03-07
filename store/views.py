ee29fcc81beaaf72e697cbd3b1530f6ff3141659
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-07 14:24:17 +0600

    I have some changes

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib import messages
 from django.views.decorators.cache import never_cache
 from django.utils.decorators import method_decorator
 
@@ -8,9 +9,11 @@ from django.utils.decorators import method_decorator
 @method_decorator(never_cache, name='dispatch')
 class HomeView(View):
     def get(self, request):
+        messages.success(request, 'Welcome to our store!')
         return render(request, 'store/home.html')
     
 class ProductView(View):
     def get(self, request):
+        messages.success(request, 'Product details page loaded!')
         return render(request, 'store/product-detail.html')
 
